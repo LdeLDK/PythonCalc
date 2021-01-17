e167c4efcbe439bf6e238e7ca2eb7e81a50f6a6b
LdeLDK <LdeLDK@gmail.com>
2021-01-17 11:17:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("""Calculator:
++---------+
+0
++---------+
+| [+] [-] |
+| [*] [/] |
++---------+
+""")
+
+print("Frist number: 558.28")
+print("Operator: +")
+print("Second number: 452.64")
+
+print("\nAnswer:")
+print("+---------+")
+print(558.28 + 452.64)
+print("+---------+")
